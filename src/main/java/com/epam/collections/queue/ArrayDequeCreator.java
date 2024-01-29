596aa2af09abadee35b9e8bb5d54ca328d9c6989
Tigran124 <tigran.h.988@gmail.com>
2024-01-29 20:26:05 +0400

    v1

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,26 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(Objects.requireNonNull(firstQueue.poll()));
+        deque.add(Objects.requireNonNull(secondQueue.poll()));
+        ArrayDeque<Integer> firstDeque = new ArrayDeque<>(firstQueue);
+        ArrayDeque<Integer> secondDeque = new ArrayDeque<>(secondQueue);
+        while (!firstDeque.isEmpty() || !secondDeque.isEmpty()) {
+            if (!firstDeque.isEmpty()) {
+                firstDeque.addLast(Objects.requireNonNull(deque.poll()));
+                deque.add(Objects.requireNonNull(firstDeque.poll()));
+                deque.add(Objects.requireNonNull(firstDeque.poll()));
+            }
+            if (!secondDeque.isEmpty()) {
+                secondDeque.addLast(Objects.requireNonNull(deque.poll()));
+                deque.add(Objects.requireNonNull(secondDeque.poll()));
+                deque.add(Objects.requireNonNull(secondDeque.poll()));
+            }
+        }
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,20 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishesToEat = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishesToEat.add(i);
+        }
+        List<Integer> result = new ArrayList<>();
+        int index = 0;
+        while (!dishesToEat.isEmpty()) {
+            index = (index + everyDishNumberToEat - 1) % dishesToEat.size();
+            result.add(dishesToEat.remove(index));
+        }
+        return result;
     }
 }
