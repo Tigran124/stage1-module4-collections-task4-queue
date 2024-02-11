5babeddbdce24138c8cc293e86db10bdb026b578
Tigran124 <tigran.h.988@gmail.com>
2024-02-11 18:27:39 +0400

    changes completed

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,17 +6,21 @@ public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
         deque.add(Objects.requireNonNull(firstQueue.poll()));
+        deque.add(Objects.requireNonNull(firstQueue.poll()));
+        deque.add(Objects.requireNonNull(secondQueue.poll()));
         deque.add(Objects.requireNonNull(secondQueue.poll()));
         ArrayDeque<Integer> firstDeque = new ArrayDeque<>(firstQueue);
         ArrayDeque<Integer> secondDeque = new ArrayDeque<>(secondQueue);
         while (!firstDeque.isEmpty() || !secondDeque.isEmpty()) {
             if (!firstDeque.isEmpty()) {
-                firstDeque.addLast(Objects.requireNonNull(deque.poll()));
+                firstDeque.addLast(Objects.requireNonNull(deque.getLast()));
+                deque.removeLast();
                 deque.add(Objects.requireNonNull(firstDeque.poll()));
                 deque.add(Objects.requireNonNull(firstDeque.poll()));
             }
-            if (!secondDeque.isEmpty()) {
-                secondDeque.addLast(Objects.requireNonNull(deque.poll()));
+            if (!secondDeque.isEmpty()){
+                secondDeque.addLast(Objects.requireNonNull(deque.getLast()));
+                deque.removeLast();
                 deque.add(Objects.requireNonNull(secondDeque.poll()));
                 deque.add(Objects.requireNonNull(secondDeque.poll()));
             }
